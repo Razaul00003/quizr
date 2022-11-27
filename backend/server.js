0285884bdede07e8e3bbb12fc3ee25a3a251cccd
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-11-27 21:51:45 +0600

    make it better

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,13 @@ app.use(cors());
 
 //-------------------------------------
 //----------deployment
-const __dirname1 = path.resolve();
+const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
