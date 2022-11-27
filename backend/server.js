31d81834611b0c925e912672865181209a96f4be
Razaul00003 <mdrazaulkarim00003@gmail.com>
2022-11-27 22:39:14 +0600

    make it better

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const authRouter = require("./routes/authRoute.js");
 //configuring environment variable
 dotenv.config({ path: "backend/config/config.env" });
 dbConnect();
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 //parse json body
 app.use(express.urlencoded({ extended: true }));
@@ -20,13 +20,13 @@ app.use(cors());
 
 //-------------------------------------
 //----------deployment
-const __dirname = path.resolve();
+const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
   });
 } else {
   app.get("/", (req, res) => {
@@ -39,5 +39,5 @@ if (process.env.NODE_ENV === "production") {
 app.use("/api", authRouter);
 app.use("/api", quizRouter);
 
-app.listen(PORT, console.log("server running"));
-console.log(PORT);
+app.listen(port, console.log("server running"));
+console.log(port);
